6e031bd3402546f61354b8106aa364b6c65eac1c
truecuozzo98 <antonio.pugliese9898@gmail.com>
2025-06-20 15:06:17 +0200

    add main.ts

--- bot.ts
+++ bot.ts
@@ -1,20 +1,22 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = require("dotenv");
-const grammy_1 = require("grammy");
+import { config } from "dotenv";
+import { Bot } from "grammy";
+
 // Carica le variabili da .env
-(0, dotenv_1.config)();
+config();
+
 // Legge il token dall'env
 const token = process.env.TELEGRAM_BOT_TOKEN;
 if (!token) {
-    console.error("TELEGRAM_BOT_TOKEN non definito in .env");
-    process.exit(1);
+  console.error("TELEGRAM_BOT_TOKEN non definito in .env");
+  process.exit(1);
 }
+
 // Crea un'istanza del bot con il token
-const bot = new grammy_1.Bot(token);
+export const bot = new Bot(token);
+
 // Gestisci il comando /start
 bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
 // Gestisci altri messaggi
 bot.on("message", (ctx) => ctx.reply("Got another message!"));
-// Avvia il bot
-bot.start();
+
+// Non avviare qui il bot con bot.start() se usi webhook!
---
+++ main.ts
@@ -0,0 +1,23 @@
+
+import { webhookCallback } from "https://deno.land/x/grammy@v1.36.3/mod.ts";
+// You might modify this to the correct way to import your `Bot` object.
+import { bot } from "./bot";
+
+const webhookUrl = `https://api.telegram.org/bot${bot.token}/setWebhook?url=https://truecuozzo9-telegram-bo-67.deno.dev/${bot.token}`;
+console.log(webhookUrl);
+
+const handleUpdate = webhookCallback(bot, "std/http");
+
+Deno.serve(async (req: { method: string; url: string | URL; }) => {
+  if (req.method === "POST") {
+    const url = new URL(req.url);
+    if (url.pathname.slice(1) === bot.token) {
+      try {
+        return await handleUpdate(req);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }
+  return new Response();
+});
